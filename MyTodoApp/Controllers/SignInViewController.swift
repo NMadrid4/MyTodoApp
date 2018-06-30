0ee6e29cfdee1571bbcd2c65e72ff15cff842096
Usuario invitado <Guest@Williams-Mac-mini.local>
2018-06-30 17:41:08 -0400

    Esconder teclado on touch

--- MyTodoApp/Controllers/SignInViewController.swift
+++ MyTodoApp/Controllers/SignInViewController.swift
@@ -9,27 +9,42 @@
 import UIKit
 
 class SignInViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var usernameTexField: UITextField!
+    @IBOutlet weak var passwordTexField: UITextField!
+    @IBOutlet weak var signinButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        usernameTexField.resignFirstResponder()
+        passwordTexField.resignFirstResponder()
+    }
+    
+    @IBAction func signinAction(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "goToHome", sender: nil)
+    }
+    
+    
+    
+    //se ejecuta al construir todos los contrains
+    //override func viewDidLayoutSubviews() {
+    
+    //}
+    
+    //dispositivo sin memoria y te da la opcion de  disparar este metodo y elminar
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
